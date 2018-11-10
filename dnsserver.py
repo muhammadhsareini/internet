23c641f688cf2c772613f5b35d2d281a6c50c287
Muhammad Sareini <mhs3vh@virginia.edu>
2018-11-09 23:49:52 -0500

    Sends response, but not working? Missing some structure to it?

--- dnsserver.py
+++ dnsserver.py
@@ -13,7 +13,7 @@ BUFFER_SIZE = 4096
 def runDNS(webServerIP):
 
 	## dns lookup table
-	table = {}
+	table = {"google.com" : "8.8.8.8", "www.google.com" : "8.8.8.8"}
 	table["blacksite.secret"] = webServerIP
 
 	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # set up udp
@@ -23,62 +23,76 @@ def runDNS(webServerIP):
 
 	while True:
 		data, addr = s.recvfrom(BUFFER_SIZE)
+		print(addr)
 		domain = parse_packet(data)
+
+		query = domain[1]
+		domain = domain[0]
+
 		if domain in table:
 			print(domain + " : " + table[domain])
+
+			## send a DNS packet response
+
+			# the dns request
+			id = query[:4]
+			# change flags for response
+			response_flags = "8580"
+			updated_response = id + response_flags + query[8:]
+			# add the "answer" part to the response
+
+			# add all info until ip
+			updated_response += "C00C0001000100000E100004"
+			# updated_response += "C00C000100010000000000000E100004"
+			# add ip address
+			ip = table[domain]
+			ip = ip.split(".")
+			
+			# convert ip to hex
+			hex_ip = ""
+			for i in ip:
+				hex_num = hex(int(i)).replace("0x","")
+				if len(hex_num) == 1:
+					hex_num = "0" + hex_num
+
+				hex_ip += hex_num
+
+			print(hex_ip)
+			updated_response+=hex_ip
+			print(updated_response)
+			packet = bytes.fromhex(updated_response.upper())
+
+			## send the packet
+			s.sendto(packet,addr)
+
 		else:
 			print(domain + " not in DNS server")
-		# print("searching domain")
-		# print(domain)
-		# print(domain == "blacksitesecret")
-		# if domain in table:
-		# 	print("found:")
-		# 	print(domain)
-		# 	ip = table[domain]
-		# 	response_packet(data, domain, ip)
-		# else:
-		# 	print("domain not found:") 
-		# 	print(domain)
 
 
 
 ## parse packet and return relevant data
 def parse_packet(data):
 	hex_form = data.hex()
-	# print("hex:")
-	# print(hex_form)
-	# print("binary:")
-	# print(bytes.fromhex(hex_form))
 	transaction_id = hex_form[0:4]
 	flags = hex_form[4:8]
 	query = hex_form[24:]
-	query += '00'
+	# query += '00'
 	# print(query)
 	domain = []
 	total = 2
+	print(query)
 	count = int(query[0:2], 16) * 2
-	# print('count', count)
 	while count != 0:
 		domain.append("".join(query[total:total+count]))
 		total += count
-		# print(total)
 		count = int(query[total:total+2], 16) * 2
 		total+=2
-		# print('count2: ', count)
-		# print(domain)
+
+	# join on ".", but represent in hex 
 	domain = "2E".join(domain)
-
 	domain = bytes.fromhex(domain).decode('utf-8')
-	return domain
-	# print("domain name", bytes.fromhex(domain).decode('utf-8'))
+	return domain, query
 
-	# return query_name
-
-
-
-## craft the response packeet 
-def response_packet(data, domain, ip):
-	return none
 
 def main(args):
     webServerIP= args[1]
