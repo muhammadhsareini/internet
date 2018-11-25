5c0ae9754e89f31bcb7bdaa5b98a1f2e06b101c6
ajayppatel <app2xr@virginia.edu>
2018-11-24 23:46:50 -0500

    IQ samples printing

--- tcp_client.py
+++ tcp_client.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 
+# for SDR must have pkg-config and libusb
+# and pyrtlsdr
+# https://witestlab.poly.edu/blog/capture-and-decode-fm-radio/
+
 import socket
 import sys
+from rtlsdr import RtlSdr
 
 TCP_IP = ''
 TCP_PORT = 4000
@@ -15,13 +20,21 @@ def runWebServer():
 	print("Running TCP Client", socket.gethostbyname(socket.gethostname()))
 
 	while True:
-		conn, addr = s.accept() #get IP and port for connecting application
+		# conn, addr = s.accept() #get IP and port for connecting application
 
-		data = conn.recv(BUFFER_SIZE)
-		print("Received data:\n", data, "\n")
+		# data = conn.recv(BUFFER_SIZE)
+		# print("Received data:\n", data, "\n")
+
+		#this behavior is weird in a while loop
+		sdr = RtlSdr()
+		sdr.sample_rate = 2.048e6
+		sdr.center_freq = 70e6
+		sdr.freq_correction = 60
+		sdr.gain = 'auto'
+		print(sdr.read_samples(512))
 
         
-		conn.close()
+		# conn.close()
 
 
 def main(args):
@@ -31,5 +44,5 @@ if __name__ == "__main__":
     main(sys.argv)
 
 #gotta figure out how this works
-def sendToSound(data):
+# def sendToSound(data):
     
